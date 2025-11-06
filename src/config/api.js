40d8099fec72d1aee29584a548581e27da98f730
Sahil Shaikh <shaikhsahil2602@gmail.com>
2025-11-07 01:17:43 +0530

    Connected frontend to live backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-export const API_BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = "https://nexax.up.railway.app";
 
 const api = axios.create({
-    baseURL:API_BASE_URL,
-    headers:{
-        "Content-Type":"application/json"
-    },
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
-
 
 export default api;
